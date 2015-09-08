2225755af88a33a52836d7833a5f9bd1c36d5bc2
mbeutel <mbeutel@cip78.informatik.uni-osnabrueck.de>
2015-09-08 13:25:58 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-unless User.find_by_email("mbeutel@uos.de")
-	admin = User.create!(email: "mbeutel@uos.de", crypted_password: "123secretPW", crypted_password_confirmation: "123secretPW", role: "admin")
-	admin.profile = Profile.create!(family_name: "Beutel", name: "Miriam")
-	admin.save!
-end
+
 u = User.find_by_email("mbeutel@uos.de")
 u.update_attributes!(role: "admin", crypted_password: "123secretPW", crypted_password_confirmation: "123secretPW")
