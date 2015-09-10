9469bbc64c7afbb7aa3153d0f7caed7275aff0c5
doschulze <doschulze@cip82.informatik.uni-osnabrueck.de>
2015-09-10 12:13:26 +0200

    fixed bootstrap stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
+//= require bootstrap-sprockets
 //= require_tree .
 $(document).ready(function() {
   $('.has-tooltip').tooltip();
