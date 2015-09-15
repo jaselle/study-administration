fcf941665a34a01371dfb65cfafe90026f043496
jselle <jselle@cip79.informatik.uni-osnabrueck.de>
2015-09-15 10:43:57 +0200

    change charts controller

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,32 +1,32 @@
+class ChartsController < ApplicationController
+  def index
+  	@blocks = Block.all
 
+  end
 
+  def sumevent(block)
+  	sumevent = 0
+  		block.events.each do |event|
+  			sumevent += event.users.count
+  		end
 
+  end
 
-
-
-
-      
-        sumevent += event.users.count
-      block.events.each do |event|
-      end
-      sumblock += sumevent(block)
-    @blocks = Block.all
-    @blocks.each do |block|
-    end
-    return sumblock
-    sumblock = 0
-    sumevent = 0
+    # execute sql query
+  def executesql(query)
       records_result = ActiveRecord::Base.connection.execute(query)
       result = records_result.getvalue(0, 0)
-    # execute sql query
     return result
+  end
+
   def block_users
-  def executesql(query)
-  def index
-  def sumevent(block)
+  	sumblock = 0
+  	@blocks.each do |block|
+  		
+  		sumblock += sumevent(block)
+  	return sumblock
+  	end
+
   end
-  end
-  end
-  end
-class ChartsController < ApplicationController
+
 end
